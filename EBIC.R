9b783e4449d2facc721ceac156c3616c2a3b0ade
Nathan Tung <ntung@Nathans-MacBook-Pro-2.local>
2022-01-23 09:52:09 -0500

    add EBIC

---
+++ EBIC.R
@@ -0,0 +1,34 @@
+# Simply computes the Gaussian log likelihood given sample covariance and estimate of precision:
+
+# Original:
+# logGaus <- function(S,K,n)
+# {
+#   SK = S %*% K
+#   tr = function(A) sum(diag(A))
+#   n/2 * (log(det(K)) - tr(SK))
+# }
+
+## According to huge???
+logGaus <- function(S,K,n)
+{
+  KS = K %*% S
+  # SK = S %*% K
+  tr = function(A) sum(diag(A))
+  return(n/2 * (determinant(K,logarithm = TRUE)$modulus - tr(KS))  )
+  # return(n/2 * (log(det(K)) - tr(SK))  )
+}
+
+# Computes the EBIC:
+EBIC <- function(S,K,n,gamma = 0.5,E,countDiagonal=FALSE)
+{
+  #   browser()
+  L <- logGaus(S, K, n)
+  if (missing(E)){
+    E <- sum(K[lower.tri(K,diag=countDiagonal)] != 0)
+    # E <- sum(abs(K[lower.tri(K,diag=countDiagonal)]) > sqrt(.Machine$double.eps))
+  }
+  p <- nrow(K)
+  
+  # return EBIC:
+  -2 * L + E * log(n) + 4 * E * gamma * log(p)  
+}
