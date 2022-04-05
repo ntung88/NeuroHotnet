a9db85726187c5d56a0c74a2c752884fce57fbcc
Nathan Tung <ntung@cs.brown.edu>
2022-04-05 16:40:53 -0400

    update runtimes to save data

--- runtimes.R
+++ runtimes.R
@@ -1,53 +1,36 @@
-setwd('~/Desktop/NeuroHotnetHD/')
-library('R.matlab')
-source('heat.R')
-source('num2name.R')
-source('simulate.R')
-source('FDR.R')
-source('naive.R')
-library('microbenchmark')
+# setwd('~/Desktop/NeuroHotnetHD/')
 
-simSC <- function(density,roi) {
-  vec = runif(floor(density * choose(roi,2)))
-  vec = c(vec,integer(choose(roi,2)-length(vec)))
-  stopifnot(length(vec) == choose(roi,2))
-  vec = sample(vec)
-  return(symMatrix(vec,roi))
-}
+mult = 50
 
-simFC <- function(prior,noisevar) {
-  load('Time Courses/tmmatrx_102513.rda')
-  tc=tm.matrx
-  rownames(tc) <- NULL
-  stds = apply(t(tc),2,sd)
-  stds = rep(stds[1:10],nrow(prior)/10)
-  test_cor = prior/max(abs(prior))
-  diag(test_cor) = 1
-  test_cor = round(test_cor,10)
-  # stopifnot(is.positive.definite(test_cor))
-  test_cov = nearPD(stds %*% t(stds) * test_cor)$mat
-  means = colMeans(t(tc))
-  means = rep(means[1:10],ncol(prior)/10)
-  dat = t(mvrnorm(n=ncol(tc),mu=means,Sigma=test_cov))
-  if (!is.null(noisevar)) {
-    noise = t(mvrnorm(n=ncol(tc),mu=rep(0,nrow(prior)),Sigma=diag(noisevar,nrow=nrow(prior))))
-    dat <- dat + noise
-  }
-  return(dat)
-}
+start = Sys.time()
 
 n=308
-r = 20
+rs = seq(from=200,to=500,by=mult)
+hottimes = vector(mode = "list", length = length(rs))
+sigtimes = vector(mode = "list", length = length(rs))
 
-sc = simSC(0.3,r)
-
-tcs = list()
-for(i in 1:n) {
-  tcs[[i]] = simFC(sc,noisevar = 500)
+for (i in 1:length(rs)) {
+  r = rs[i]
+  sc = simSC(0.3,r)
+  
+  tcs = list()
+  for(j in 1:n) {
+    tcs[[j]] = simFC(sc,noisevar = 500)
+  }
+  
+  hotnet = microbenchmark(FDR(tcs,sc,0.05,10,1000,0.1),times=10)
+  hottimes[[i]] = hotnet$time
+  siggm = microbenchmark(SiGGM(tcs,sc,0.11),times=10)
+  sigtimes[[i]]= siggm$time
+  
+  save(hottimes,file='hottimes.rda')
+  save(sigtimes,file='sigtimes.rda')
 }
 
-hotnet = microbenchmark(FDR(tcs,sc,0.05,10,1000,0.1),times=1)
-siggm = microbenchmark(SiGGM(tcs,sc,0.11),times=1)
+# pdf(file='runtimes.pdf')
+# plot(rs,hottimes)
+# par(new=TRUE)
+# plot(rs,sigtimes)
+# dev.off()
 
-print(hotnet)
-print(siggm)
+print(Sys.time()-start)
